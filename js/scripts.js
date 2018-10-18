e9cc58e03413bf0e7ddc6ee6c922e29edc64cafc
Blake Gotting, Cole Marsteller and Devin Sweeting <blake@>
2018-10-18 11:16:45 -0700

    Create website for amusement park to interpret heights.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
+  var height = parseInt(prompt("Height in inches:"));
+
+  if (height >= 49 && height <= 77) {
+    $('#tall').show();
+  } else if (height > 77) {
+    alert("calm down");
+    $('#tooTall').show();
+  } else {
+    $('#short').show();
+  }
+
 
 });
